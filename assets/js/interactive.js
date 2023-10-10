730517d85915bedb5993ecd6999a974425e1ddfc
leedthanh <leedthanh@gmail.com>
2023-10-10 01:43:56 -0500

    update maps and slogan

--- assets/js/interactive.js
+++ assets/js/interactive.js
@@ -7,6 +7,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(htown_map);
 
+htown_map.invalidateSize();
+
 const stateAbbreviationToName = {
 
     "AL": "Alabama",
